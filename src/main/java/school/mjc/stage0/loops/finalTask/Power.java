8bfdc9e9fbfd2f310625d6bf112659bdf87b25ec
Ivan Lapunka <ivan_lapunka@gmail.com>
2023-03-02 22:57:58 +0300

    Changes

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        if (numberToPrint == 0) {
+            System.out.println(0);
+            return;
+        }
+        int newPower = 1;
+        for (int i = 0; i < power; i++) {
+            newPower *= numberToPrint;
+        }
+        System.out.println(newPower);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int middle = cathetusLength;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength * 2 - 1; j++) {
+                if ( middle - i < j && j < middle + i) {
+                    System.out.print(Math.abs(middle - j) + 1);
+                } else if (j >= middle + i) {
+                    break;
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
